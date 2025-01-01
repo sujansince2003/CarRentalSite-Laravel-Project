cc4a9f607bb870622a825b7b1dff5245f5c2232f
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-01-01 23:46:47 +0545

    defined route for home,login and signup

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SignupController;
+use App\Http\Controllers\loginController;
 
 Route::get('/', [HomeController::class,"index"]
 );
+
+Route::get("/signup",[SignupController::class,"create"]);
+
+Route::get("/login",[loginController::class,"createLoginPage"]);
